3f1fcdbb22c4495aa88bba949f50cefc8d8ead53
h.toriyama <toriyamahiroto1128@gmail.com>
2023-05-06 13:42:14 +0900

    静的ファイル＆favicon設定

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.routers import api
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 
 app = FastAPI()
@@ -14,3 +16,9 @@ app.add_middleware(
 )
 
 app.include_router(api.router, prefix="/api")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("static/favicon.ico")
